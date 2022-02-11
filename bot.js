bff041f6d7b6460992ed59be9328e031cfd2b11d
Battle Chicken <felix@larkov.de>
2022-02-10 19:59:59 -0500

    "added options to the play command"

--- bot.js
+++ bot.js
@@ -6,6 +6,27 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
+
+const nodes = [
+	{
+	  host: "localhost",
+	  password: "youshallnotpass",
+	  port: 2333,
+	}
+  ];
+  
+  // Assign Manager to the client variable
+  client.manager = new Manager({
+	// The nodes to connect to, optional if using default lavalink options
+	nodes,
+	// Method to send voice data to Discord
+	send: (id, payload) => {
+	  const guild = client.guilds.cache.get(id);
+	  // NOTE: FOR ERIS YOU NEED JSON.stringify() THE PAYLOAD
+	  if (guild) guild.shard.send(payload);
+	}
+  });
+
 //Event Handler
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 console.log('-----Loading Events-----')
@@ -36,4 +57,19 @@ for (const dir of commandDirs) {
 
 
 //-----Erela events-----
+
+// Emitted whenever a node connects
+client.manager.on("nodeConnect", node => {
+    console.log(`Node "${node.options.identifier}" connected.`)
+})
+
+// Emitted whenever a node encountered an error
+client.manager.on("nodeError", (node, error) => {
+    console.log(`Node "${node.options.identifier}" encountered an error: ${error.message}.`)
+})
+
+// THIS IS REQUIRED. Send raw events to Erela.js
+client.on("raw", d => client.manager.updateVoiceState(d));
+
+
 client.login(token);
--- commands/music/play.js
+++ commands/music/play.js
@@ -6,9 +6,16 @@ module.exports = {
 	data:
 		new SlashCommandBuilder()
 			.setName('play')
-			.setDescription('Plays a song'),
-
+			.setDescription('Plays a song')
+			.addStringOption(option =>
+				option.setName("song")
+					.setDescription("The song you want to play")
+					.setRequired(true)),
 	async execute(interaction) {
+		const search = interaction.options.getString("song")
+		console.log(search)
+
 		//If I had code it would go here
+
 	},
 };
