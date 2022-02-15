2bddc5ab0e5528ce14649c5673feef54d0b6b112
Battle Chicken <felix@larkov.de>
2022-02-14 21:01:05 -0500

    Added some basica erela infrastructure

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,14 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 module.exports = {
 	category: "music",
 	usage: "/play <song>",
 	data:
 		new SlashCommandBuilder()
 			.setName('play')
-			.setDescription('Plays a song'),
+			.setDescription('Plays a song')
+			.addStringOption(option =>
+				option.setName("song")
+					.setDescription("The song you want to play")
+					.setRequired(true)),
+	async execute(client, interaction) {
+		const search = interaction.options.getString("song")
+		let res
+		console.log(search)
+		try {
+			res = await client.manager.search(search, interaction.author);
+			if (res.loadType === "LOAD_FAILED") throw res.exception;
+			else if (res.loadType === "PLAYLIST_LOADED") throw { message: "Playlists are not supported with this command." };
+		} catch (err) {
+			interaction.reply("There was an error while searching.")
+		}
+		const player = client.manager.create({
+			guild: interaction.guild.id,
+			voiceChannel: interaction.member.voice.channel.id,
+			textChannel: interaction.channel.id,
+		  });
+		  player.connect();
+		  player.queue.add(res.tracks[0]);
+		  if (!player.playing && !player.paused && !player.queue.size) player.play()
+		  return interaction.reply(`queuing ${res.tracks[0].title}.`)
 
-	async execute(interaction) {
 		//If I had code it would go here
+
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (!command) return;
 
 		try {
-		await command.execute(interaction);
+		await command.execute(client, interaction);
 		} catch (error) {
 		console.error(error);
 		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
